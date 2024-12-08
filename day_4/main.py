90023ecb7734ec647541cb1b5a96317f8864ac78
João Vitor de Camargo <camargo.dev@gmail.com>
2024-12-08 18:48:49 -0300

    Part 1 working

--- day_4/main.py
+++ day_4/main.py
@@ -2,6 +2,6 @@ from src.part_1.XmasFinder import Xmasfinder as FirstXmasfinder
 from src.part_2.XmasFinder import Xmasfinder as SecondXmasfinder
 
 if __name__ == "__main__":
-    lines = [line.replace("\n","") for line in open("day_4/res/example.txt", "r")]
+    lines = [line.replace("\n","") for line in open("day_4/res/input.txt", "r")]
     print(FirstXmasfinder().find(lines))
     print(SecondXmasfinder().find(lines))
