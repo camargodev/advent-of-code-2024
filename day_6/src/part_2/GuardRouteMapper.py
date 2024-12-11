cdc3daf017a34ecb999f3d0fb78d3a1ec8c96b63
João Vitor de Camargo <camargo.dev@gmail.com>
2024-12-10 21:46:09 -0300

    Only visiting cells in original path

--- day_6/src/part_2/GuardRouteMapper.py
+++ day_6/src/part_2/GuardRouteMapper.py
@@ -1,4 +1,5 @@
-from collections import defaultdict
+from collections import deque
+
 
 OBSTACLE = "#"
 GUARD_START = "^"
@@ -9,36 +10,34 @@ RIGHT = (0, +1)
 
 class Guardroutemapper:
     def map(self, lines):
-        num_rows, num_cols, map_positions, obstacles, start, direction = self.extract_map_data(lines)
-        next_obstacles_by_direction = self.get_next_obstacles(num_rows, num_cols, obstacles)
-        guard_in_loop_count = 0
-        loop, visited = self.is_guard_stuck_on_loop(map_positions, obstacles, None, start, direction, next_obstacles_by_direction)
-        return len(visited)
-        for index, additional_obstacle in enumerate(map_positions):
-            if additional_obstacle in obstacles or additional_obstacle == start:
-                continue
-            print(index, additional_obstacle, len(map_positions))
-            found_loop = self.is_guard_stuck_on_loop(map_positions, obstacles, additional_obstacle, start, direction, next_obstacles_by_direction)
-            if found_loop:
-                guard_in_loop_count += 1
-        return guard_in_loop_count
+        map_positions, obstacles, position, direction = self.extract_map_data(lines)
+        _, movements = self.run_trough_path( map_positions, obstacles, position, direction)
+        visited_cells = set([position for position, _ in movements])
+        number_of_loops = 0
+        for index, cell in enumerate(visited_cells):
+            print(index, len(visited_cells))
+            stuck_in_loop, _ = self.run_trough_path( map_positions, obstacles, position, direction, add_obstacle=cell)
+            if stuck_in_loop:
+                number_of_loops += 1
+        return number_of_loops
     
-    def is_guard_stuck_on_loop(self, map_positions, obstacles, additional_obstacle, start, direction, next_obstacles_by_direction):
+    def run_trough_path(self,  map_positions, obstacles, position, direction, add_obstacle=None):
         visited_movements = set()
-        visited_movements.add((start, direction))
-        position = start
+        visited_movements.add((position, direction))
         while position is not None:
-            next_obstacle = next_obstacles_by_direction[direction].get(position, None)
-            if next_obstacle is None: 
-                return False, set([position for position, _ in visited_movements])
-            last_before_obstacle = self.get_previous(position, self.get_opposite(direction))
-            if (last_before_obstacle, direction) in visited_movements:
-                return True, set([position for position, _ in visited_movements])
-            direction = self.rotate(direction)
-            position = last_before_obstacle
-            visited_movements.add((position, direction))
-        visited_cells = set([position for position, _ in visited_movements])
-        return False, visited_cells
+            next = self.get_next(position, direction)
+            print(position, direction)
+            if (next, direction) in visited_movements:
+                return True, visited_movements
+            if next not in map_positions:
+                position = None
+                continue
+            if next in obstacles or next == add_obstacle:
+                direction = self.rotate(direction)
+                next = self.get_next(position, direction)
+            visited_movements.add((next, direction))
+            position = next
+        return False, visited_movements
 
     def rotate(self, direction):
         if direction == UP:
@@ -48,79 +47,21 @@ class Guardroutemapper:
         if direction == DOWN:
             return LEFT
         return UP
-    
-    def get_opposite(self, direction):
-        if direction == UP:
-            return DOWN
-        if direction == DOWN:
-            return UP
-        if direction == RIGHT:
-            return LEFT
-        return RIGHT
 
     def get_next(self, position, direction):
         row, col = position
         row_offset, col_offset = direction
         return row+row_offset, col+col_offset
-    
-    def get_previous(self, position, direction):
-        row, col = position
-        row_offset, col_offset = direction
-        return row-row_offset, col-col_offset
-    
-    def get_next_obstacles(self, num_rows, num_cols, obstacles):
-        next_obstacles_by_direction = dict()
-        next_obstacle_left = dict()
-        next_obstacle_right = dict()
-        next_obstacle_down = dict()
-        next_obstacle_up = dict()
-        for row in range(num_rows):
-            last_obstacle_left = None
-            for col in range(num_cols):
-                if (row, col) in obstacles:
-                    last_obstacle_left = (row, col)
-                if last_obstacle_left is not None:
-                    next_obstacle_left[(row, col)] = last_obstacle_left
-            next_obstacles_by_direction[LEFT] = next_obstacle_left
-
-            last_obstacle_right = None
-            for col in range(num_cols, -1, -1):
-                if (row, col) in obstacles:
-                    last_obstacle_right = (row, col)
-                if last_obstacle_right is not None:
-                    next_obstacle_right[(row, col)] = last_obstacle_right
-            next_obstacles_by_direction[RIGHT] = next_obstacle_right
-
-        for col in range(num_cols):
-            last_obstacle_down = None
-            for row in range(num_rows, -1, -1):
-                if (row, col) in obstacles:
-                    last_obstacle_down = (row, col)
-                if last_obstacle_down is not None:
-                    next_obstacle_down[(row, col)] = last_obstacle_down
-            next_obstacles_by_direction[DOWN] = next_obstacle_down
-
-            last_obstacle_up= None
-            for row in range(num_rows):
-                if (row, col) in obstacles:
-                    last_obstacle_up = (row, col)
-                if last_obstacle_up is not None:
-                    next_obstacle_up[(row, col)] = last_obstacle_up
-            next_obstacles_by_direction[UP] = next_obstacle_up
-
-        return next_obstacles_by_direction
 
     def extract_map_data(self, lines):
         initial_position = None
-        map_positions = dict()
+        map_positions = set()
         obstacles = set()
-        num_rows = len(lines)
-        num_cols = len(lines[0])
         for row, line in enumerate(lines):
             for col, value in enumerate(line):
-                map_positions[(row, col)] = value
+                map_positions.add((row, col))
                 if value == OBSTACLE:
                     obstacles.add((row, col))
                 if value == GUARD_START:
                     initial_position = (row, col)
-        return num_rows, num_cols, map_positions, obstacles, initial_position, UP 
+        return map_positions, obstacles, initial_position, UP 
